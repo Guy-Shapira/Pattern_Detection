fixed training itreation, softmax overflow error and decided on RL strcture

Model/model_OpenCEP.py
@@ -134,9 +134,14 @@ with torch.autograd.detect_anomaly():
             # print("finished training dir creation!")
 
             # self.critic_optimizer = torch.optim.Adam(self.actor_critic.critic.parameters(), lr=lr_actor)
-            self.critic_optimizer = torch.optim.SGD(self.actor_critic.critic.parameters(), lr=lr_actor)
+            params = list(self.actor_critic.actor.parameters()) + list(self.actor_critic.critic.parameters())
+
+            self.critic_optimizer = torch.optim.SGD(params, lr=lr_critic)
             # self.actor_optimizer = torch.optim.Adam(self.actor_critic.actor.parameters(), lr=lr_critic)
-            self.actor_optimizer = torch.optim.SGD(self.actor_critic.actor.parameters(), lr=lr_critic)
+            # print(type(self.actor_critic.actor.parameters()))
+            # print(self.actor_critic.actor.parameters() + self.actor_critic.critic.parameters())
+            # exit(0)
+            self.actor_optimizer = torch.optim.SGD(self.actor_critic.actor.parameters(), lr=lr_actor)
             self.all_cols = all_cols
             self.cols = eff_cols
             self.max_fine_app = max_fine_app
@@ -358,8 +363,8 @@ with torch.autograd.detect_anomaly():
                 raise Exception("Data set not supported")
 
         def forward(self, input, old_desicions, mask, training_factor=0.0, T=1):
-            _, event_after_softmax = self.actor_critic.forward_actor(input, old_desicions, mask, training_factor, T)
-            value_reward, value_rating = self.actor_critic.forward_critic(input, old_desicions,  mask, training_factor, T)
+            base_value, event_after_softmax = self.actor_critic.forward_actor(input, old_desicions, mask, training_factor, T)
+            value_reward, value_rating = self.actor_critic.forward_critic(input, old_desicions, base_value, mask, training_factor, T)
             return event_after_softmax, value_reward, value_rating
 
         def get_event(self, input, old_desicions, index=0, mask=None, training_factor=0.0, T=1):
@@ -442,10 +447,10 @@ with torch.autograd.detect_anomaly():
             total_entropy = 0
             comps_to = []
 
-            after_relu, _ = self.actor_critic.forward_actor(data, old_desicions.cuda(), None, training_factor)
-            value_reward, value_rating = self.actor_critic.forward_critic(data, old_desicions.cuda(), None, training_factor)
+            base_value, _ = self.actor_critic.forward_actor(data, old_desicions.cuda(), None, training_factor)
+            value_reward, value_rating = self.actor_critic.forward_critic(data, old_desicions.cuda(), base_value, None, training_factor)
             for i in range(self.num_cols):
-                action, value, log, entropy = self.single_col_mini_action(after_relu, i, training_factor) #this is weird, should update data after actions
+                action, value, log, entropy = self.single_col_mini_action(base_value, i, training_factor) #this is weird, should update data after actions
                 mini_actions_vals.append(action)
                 total_entropy += entropy / self.num_cols
                 mini_action, cond, comp_to = get_action_type(action, self.num_actions, self.actions, self.match_max_size)
@@ -526,7 +531,14 @@ with torch.autograd.detect_anomaly():
     #     return actor_loss_value, critic_loss_value
 
     def update_policy(policy_network, ratings, rewards, log_probs, values_rating, values_reward,
-                    Qval_rating, Qval_reward, entropy_term, epoch_idx,  flag=False):
+                    Qval_rating, Qval_reward, entropy_term, epoch_idx, flag=False):
+
+        def l1_penalty(log_probs, l1_lambda=0.001):
+            """
+            Returns the L1 penalty of the params.
+            """
+            l1_norm = sum(log_prob.abs().sum() for log_prob in log_probs)
+            return l1_lambda*l1_norm / len(log_probs)
 
         Qvals_reward = np.zeros_like(values_reward)
         Qvals_rating = np.zeros_like(values_rating)
@@ -540,8 +552,14 @@ with torch.autograd.detect_anomaly():
         Qvals = torch.FloatTensor((Qvals_reward, Qvals_rating)).requires_grad_(True)
         log_probs = torch.stack(log_probs).requires_grad_(True)
         advantage = Qvals - values
+        log_probs_reg = l1_penalty(log_probs, l1_lambda=0.05)
 
         actor_loss = (-log_probs * advantage).mean().requires_grad_(True)
+        print("----------\n")
+        print(actor_loss, log_probs_reg)
+        print("----------\n")
+
+        actor_loss += log_probs_reg
         critic_loss = 0.5 * advantage.pow(2).mean().requires_grad_(True)
         policy_network.actor_optimizer.zero_grad()
         policy_network.critic_optimizer.zero_grad()
@@ -569,7 +587,7 @@ with torch.autograd.detect_anomaly():
     def train(model, num_epochs=5, test_epcohs=False, split_factor=0, bs=0, rating_flag=True):
         # run_name = "second_level_setup_all_lr" + str(model.lr)
         # run_name = f"StarPilot Exp! fixed window, window_size = {model.window_size} attention = 2.5"
-        run_name = f"logging losses updated learning rates- SGD"
+        run_name = f"bigger actor-critic phases"
         not_finished_count = 0
         run = wandb.init(project='Pattern_Mining', entity='guyshapira', name=run_name, settings=wandb.Settings(start_method='fork'))
         config = wandb.config
@@ -678,9 +696,9 @@ with torch.autograd.detect_anomaly():
                     )
                     normalize_rating, normalize_reward = [], []
                     mask = torch.tensor([1.0] * (model.num_events + 1))
-                    if in_round_count % 35 == 0 and epoch < 5:
-                        temper /= 1.05
-                    if total_count % 25 == 0:
+                    # if in_round_count % 35 == 0 and epoch < 5:
+                    #     temper /= 1.05
+                    if total_count % 250 == 0 and training_factor > 0.3:
                         training_factor /= 1.2
                     while not is_done:
                         if not set_data is None:
@@ -711,12 +729,12 @@ with torch.autograd.detect_anomaly():
 
                         if action == model.num_events:
                             ratings.append(1)
+                            log_probs.append(log_prob)
+
                             if len(actions) == 0:
-                                log_probs.append(log_prob)
                                 rewards.append(-1.5)
                                 real_rewards.append(-1.5)
                             else:
-                                log_probs.append(log_prob)
                                 rewards.append(10)
                                 real_rewards.append(10)
                                 break
@@ -732,7 +750,8 @@ with torch.autograd.detect_anomaly():
                             for j, action_val in enumerate(actions_vals):
                                 old_desicions = old_desicions.clone()
                                 old_desicions[count * (model.num_cols + 1) + j + 1] = model.embedding_actions(torch.tensor(action_val))
-                            log_prob += log.item()
+                            
+                            log_prob = (log_prob + log.item()) / 2
                             log_probs.append(log_prob)
                             actions.append(mini_actions)
 
@@ -867,17 +886,17 @@ with torch.autograd.detect_anomaly():
                         send_rewards = real_rewards
 
                     actor_flag = False
-                    if num_epochs_trained <= 6:
-                        if count_actor < 20:
-                            actor_flag = True
-                            count_actor += 1
-                        elif count_critic < 50:
-                            count_critic += 1
-                        else:
-                            count_actor = 0
-                            count_critic = 0
-                    else:
+                    # if num_epochs_trained <= 6:
+                    if count_actor < 100:
                         actor_flag = True
+                        count_actor += 1
+                    elif count_critic < 250:
+                        count_critic += 1
+                    else:
+                        count_actor = 0
+                        count_critic = 0
+                    # else:
+                    #     actor_flag = True
 
                     a1, c1  = update_policy(model, ratings, real_rewards, log_probs, values_rating, values_reward,
                                                                     Qval_rating, Qval_reward,
@@ -908,7 +927,8 @@ with torch.autograd.detect_anomaly():
                         if (real_rewards[index_max] > 2 or random.randint(0,3) > 1) or (ratings[index_max] > 2 or random.randint(0,3) > 1):
                             wandb.log({"reward": real_rewards[index_max], "rating": ratings[index_max],
                                     "max rating": np.max(ratings), "actor_flag": int(actor_flag),
-                                    "actor_loss_reward": a1, "critic_loss_reward": c1})
+                                    "actor_loss_reward": a1, "critic_loss_reward": c1,
+                                    "curent_step": total_steps_trained})
 
                         # if total_steps_trained > 4500:
                         #     # Only for sweeps!
@@ -1263,7 +1283,7 @@ with torch.autograd.detect_anomaly():
         parser = argparse.ArgumentParser(description='CEP pattern miner')
         parser.add_argument('--bs', default=800, type=int, help='batch size')
         parser.add_argument('--epochs', default=20, type=int, help='num epochs to train')
-        parser.add_argument('--lr_actor', default=5e-7, type=float, help='starting learning rate for actor')
+        parser.add_argument('--lr_actor', default=5e-8, type=float, help='starting learning rate for actor')
         parser.add_argument('--lr_critic', default=5e-6, type=float, help='starting learning rate for critic')
         parser.add_argument('--hidden_size1', default=1024, type=int, help='hidden_size param for model')
         parser.add_argument('--hidden_size2', default=2048, type=int, help='hidden_size param for model')

Model/models.py
@@ -22,6 +22,7 @@ from Model.utils import (
     run_OpenCEP,
     check_predictor,
     calc_near_windows,
+    OverFlowError,
 )
 
 from Model.rating_module import (
@@ -32,7 +33,6 @@ from Model.rating_module import (
 
 import tqdm
 import pathlib
-from bayes_opt import BayesianOptimization
 import ast
 import sys
 import time
@@ -53,7 +53,6 @@ import pandas as pd
 from stream.FileStream import FileInputStream, FileOutputStream
 from sklearn.neighbors import KNeighborsClassifier as KNN
 import wandb
-from bayes_opt import BayesianOptimization
 import json
 from torch.optim.lr_scheduler import StepLR
 
@@ -95,20 +94,16 @@ class ModelBase(nn.Module):
         return after_relu
 
 
-class Critic(ModelBase):
-    def __init__(self, hidden_size1, hidden_size2,
-                embeddding_total_size, window_size, match_max_size,
-                num_cols, num_actions, num_events):
-        super().__init__(hidden_size1, hidden_size2,
-                    embeddding_total_size, window_size, match_max_size,
-                    num_cols, num_actions, num_events)
+class Critic(nn.Module):
+    def __init__(self, hidden_size2):
+        super(Critic, self).__init__()
+        self.hidden_size = hidden_size2
 
-        self.critic_reward = nn.Linear(self.hidden_size2, 1).cuda()
-        self.critic_rating = nn.Linear(self.hidden_size2, 1).cuda()
+        self.critic_reward = nn.Linear(self.hidden_size, 1).cuda()
+        self.critic_rating = nn.Linear(self.hidden_size, 1).cuda()
 
 
-    def forward(self, input, old_desicions, mask, training_factor, T):
-        base_output = self.forward_base(input, old_desicions, mask, training_factor, T)
+    def forward(self, input, old_desicions, base_output,  mask, training_factor, T):
         value_reward = self.critic_reward(base_output)
         value_rating = self.critic_rating(base_output)
         return value_reward, value_rating
@@ -134,7 +129,8 @@ class Actor(ModelBase):
     def forward(self, input, old_desicions, mask, training_factor, T):
         def masked_softmax(vec, mask, dim=1, T=1, epsilon=1e-5):
             vec = vec / T
-            exps = torch.exp(vec).cpu()
+            normalized_vec = vec - torch.max(vec)
+            exps = torch.exp(normalized_vec).cpu()
             masked_exps = exps * mask.float()
             masked_sums = masked_exps.sum(dim, keepdim=True) + epsilon
             check_sums = masked_sums.cpu().detach().numpy()
@@ -143,11 +139,12 @@ class Actor(ModelBase):
                 check_toghter = check_exps/check_sums
             except Exception as e:
                 print("anomaly- extreamly large value!")
-                #TODO: raise a unique error- to end training!
-                base_array = torch.zeros_like(masked_exps).to(masked_exps.device)
-                max_index = torch.argmax(masked_exps).item()
-                base_array[max_index] = 1.0
-                return base_array
+                #TODO: raise a unique error- to end training! - Done
+                raise OverFlowError(vec, mask, T)
+                # base_array = torch.zeros_like(masked_exps).to(masked_exps.device)
+                # max_index = torch.argmax(masked_exps).item()
+                # base_array[max_index] = 1.0
+                # return base_array
                 
             return (masked_exps/masked_sums)
 
@@ -231,21 +228,14 @@ class ActorCriticModel(nn.Module):
 
     def _create_critic(self):
         return Critic(
-            hidden_size1=self.hidden_size1,
             hidden_size2=self.hidden_size2,
-            embeddding_total_size=self.embeddding_total_size,
-            window_size=self.window_size,
-            match_max_size=self.match_max_size,
-            num_cols=self.num_cols,
-            num_actions=self.num_actions,
-            num_events=self.num_events
         )
 
     def forward_actor(self, input, old_desicions, mask=None, training_factor=0.0, T=1):
         return self.actor.forward(input, old_desicions, mask, training_factor, T)
 
-    def forward_critic(self, input, old_desicions, mask=None, training_factor=0.0, T=1):
-        return self.critic.forward(input, old_desicions, mask, training_factor, T)
+    def forward_critic(self, input, old_desicions, base_output, mask=None, training_factor=0.0, T=1):
+        return self.critic.forward(input, old_desicions, base_output, mask, training_factor, T)
 
     def forward_actor_mini_actions(self, index, data, mask, training_factor):
         return self.actor.forward_mini_actions(index, data, mask, training_factor)